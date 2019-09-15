d773bd11dcb241e119201c1bd876b84cf8368233
dimitar.nedilkovski@gmail.com <dimitar.nedilkovski@gmail.com>
2019-09-15 11:54:07 +0300

    Add  MainDeck struct and add metohod which generate deck

---
+++ BlackJack2.0/MainDeck.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackJack2._0
+{
+    public struct MainDeck
+    {
+        public int UserHand;
+
+        public  Card[] CreateDeck()
+        {
+            Card[] deck = new Card[36];
+            for (int cardName = 2, index=0; cardName<=11;cardName++)
+            {
+                if (cardName != 5)
+                {
+                    for(int cardSuit = 0; cardSuit <= 3; cardSuit++, index++)
+                    {
+                        deck[index]= new Card((CardName)cardName,(Suit)cardSuit);
+                    }
+                }
+            }
+            return deck;
+        }
+    }
+}
