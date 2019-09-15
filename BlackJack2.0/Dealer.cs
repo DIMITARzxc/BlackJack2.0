93e7d4324bdd781d02299acbb53c4f84b6771fb4
dimitar.nedilkovski@gmail.com <dimitar.nedilkovski@gmail.com>
2019-09-15 12:07:08 +0300

    Add two structs like dealer and player

---
+++ BlackJack2.0/Dealer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackJack2._0
+{
+     public struct Dealer
+    {
+        public int TotalScore;
+        public int Win;
+        public string Information;
+        
+        public int GetDealerCard(Card[] deck, int usedHand,)
+        {
+
+        }
+    }
+}
